e8bcefa6e0458a1623bd1f87c229660ba5b071a6
ICantAffordWave <tomasxd126@gmail.com>
2026-09-09 16:10:40 -0300

    Update thescriptmate.lua

--- thescriptmate.lua
+++ thescriptmate.lua
@@ -72,7 +72,7 @@ local args = {
 	"-net",
 }
 game:GetService("ReplicatedStorage"):WaitForChild("01_server"):FireServer(unpack(args))
-warn("btw for some reason github kinda weird i always get nil sometimes with the reanimate, if it does just wait till github fixes itself, i'm getting 503 (sometimes) so prob internal issue i dont have anything to do with this, the reanimate is by tory + myworld")
+warn("btw for some reason github kinda weird i always get nil sometimes with the reanimate, if it does just wait till github fixes itself, i'm getting 503 so prob internal issue i dont have anything to do with this, the reanimate is by tory + myworld, and if it happens to you, just wait i dont know")
 loadstring(httpget("https://raw.githubusercontent.com/Jskfhggjxu/My-Script/refs/heads/main/Cool-Reanimate.lua"))()
 task.wait(3)
 local args = {
